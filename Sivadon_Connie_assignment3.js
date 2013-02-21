dae4439cc602dc181cbbe05ee9ff90fbaba65f86
csivadon <csivadon@fullsail.edu>
2013-02-21 09:13:24 +0100

    wrote my json information.

--- Sivadon_Connie_assignment3.js
+++ Sivadon_Connie_assignment3.js
@@ -1,1 +1,53 @@
-alert("JavaScript works!");
+// Connie Sivadon
+// SDI 1302
+// Project 2
+// 21 February 2013
+
+// main code variables
+var jsonClanInfo = {
+	"clanName"			 : "Enthusiastic Daddlers Online",
+	"clanTag" 			 : "edo",
+	"numberOfActiveMemb" : 9,
+	"clanMemberInfo" : [
+		{
+			"name" 			 : "Patrick",
+			"clanPosition"   : "Leader",
+			"memberForYears" : 7
+		},{
+			"name" 			 : "Connie",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 6
+		},{
+			"name" 			 : "Michael",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 6
+		},{
+			"name"			 : "Chris",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 3
+
+		},{
+			"name" 			 : "Andre",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 3
+		},{
+			"name" 			 : "Stephan",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 3
+		},{
+			"name" 			 : "Danny",
+			"clanPosition"	 : "Member",
+			"memberForYears" : 1
+		},{
+			"name"			 : "Jessy",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 1
+		},{
+			"name" 			 : "Carsten",
+			"clanPosition" 	 : "Member",
+			"memberForYears" : 2
+		},
+	] // clanMemberInfo
+}; //jsonClanInfo
+
+console.log (jsonClanInfo);
